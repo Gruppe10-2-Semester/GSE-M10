ba026659a1d08a4218bc75703eb8c38085d36c7e
Xrosby <mads.sfg@gmail.com>
2017-04-20 09:27:32 +0200

    Register

---
+++ GSE-M10/src/gse/m10/Register.java
@@ -0,0 +1,17 @@
+package gse.m10;
+
+/**
+ * Created by MadsNorby on 20/04/17.
+ */
+public class Register {
+
+    private ProductCatalog productCatalog;
+    private Sale currentSale;
+
+
+    public void makeNewSale(){}
+    public void enterItem(String itemID, int quantity) {}
+    public void endSale() {}
+    public void makePayment(double amount) {}
+
+}
