2426f330ba733cb9e502cdc8d110d26cf7e98c9a
Xrosby <mads.sfg@gmail.com>
2017-04-20 10:13:03 +0200

    Store

--- GSE-M10/src/gse/m10/Store.java
+++ GSE-M10/src/gse/m10/Store.java
@@ -7,4 +7,16 @@ public class Store {
 
     private String adress;
     private String name;
+    private ProductCatalog productCatalog;
+
+    public Store(String adress, String name) {
+        this.adress = adress;
+        this.name = name;
+        this.productCatalog = new ProductCatalog();
+    }
+
+
+
+
+
 }
